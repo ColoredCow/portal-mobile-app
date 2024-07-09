6cd203d3ce0ed598481e7fecb071ed8f504147b5
AbhishekNegi25 <pankaj.agrawal@coulomb.ai>
2024-07-09 13:58:48 +0530

    setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,30 +2,19 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
-import Login from './login';
-import Home from './home';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loggedIn, setLoggedIn] = useState<boolean | null>(null);
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      const token = await AsyncStorage.getItem('userToken');
-      setLoggedIn(token !== null);
-    };
-
-    checkLoginStatus();
-  }, []);
 
   useEffect(() => {
     if (loaded) {
@@ -33,26 +22,16 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  const handleLogout = async () => {
-    try {
-      await AsyncStorage.clear();
-      setLoggedIn(false);
-    } catch (e) {
-      console.error('Failed to clear the async storage', e);
-    }
-  };
-
-  if (!loaded || loggedIn === null) {
+  if (!loaded) {
     return null;
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      {!loggedIn ? (
-        <Login setLoggedIn={setLoggedIn} />
-      ) : (
-        <Home onLogout={handleLogout} />
-      )}
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
     </ThemeProvider>
   );
 }
